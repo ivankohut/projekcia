40e38d995a9cddc73d2334de5516d9bedd823cc0
Ivan Kohut <ivankohut@azet.sk>
2025-04-03 08:25:47 +0200

    Presun/premenovanie niektorých súborov/adresárov

--- opensong/create-opensong-archive.sh
+++ opensong/create-opensong-archive.sh
@@ -1,23 +1,24 @@
 #!/bin/bash -e
 
-# Creates 7z archive of configured OpenSong with all standard song databases (Chvaly, 400, ...) and slovak translations of Bible (Ekumenicky, Evanjelicky, Rohackov).
+# Creates 7z archive of configured OpenSong with all standard hymn books (Chvaly, 400, ...) and slovak translations of Bible (Ekumenicky, Evanjelicky, Rohackov).
 
-REPOS_DIR=../git
+HYMN_BOOKS_DIR=../spevniky
+BIBLES_DIR=../biblie/opensong-bibles
 
 function copySongDatabase {
 	local SOURCE_DIR_NAME="$1"
 	local DESTINATION_DIR_NAME="$2"
 	local DESTINATION="$OPENSONG_DIR/OpenSong Data/Songs/$DESTINATION_DIR_NAME"
 	mkdir -p "$DESTINATION"
-	cp "$REPOS_DIR/$SOURCE_DIR_NAME/"* "$DESTINATION"
+	cp "$HYMN_BOOKS_DIR/$SOURCE_DIR_NAME/"* "$DESTINATION"
 }
 
 function copyBible {
 	local BIBLE_NAME="$1"
 	local DESTINATION="$OPENSONG_DIR/OpenSong Scripture"
 	mkdir -p "$DESTINATION/indexes"
-	cp "$REPOS_DIR/opensong-bibles/$BIBLE_NAME" "$DESTINATION"
-	cp "$REPOS_DIR/opensong-bibles/indexes/$BIBLE_NAME.ind" "$DESTINATION/indexes"
+	cp "$BIBLES_DIR/$BIBLE_NAME" "$DESTINATION"
+	cp "$BIBLES_DIR/indexes/$BIBLE_NAME.ind" "$DESTINATION/indexes"
 }
 
 WORK_DIR=$(mktemp -d)
@@ -42,7 +43,7 @@ copyBible "SK Roháčkov"
 
 # Pack everything into 7Z archive
 rm -f opensong.7z
-7z a -l -mx=9 opensong.7z "$OPENSONG_DIR"
+7z a -mx=9 opensong.7z "$OPENSONG_DIR"
 
 # Clean up
 rm -r $WORK_DIR
