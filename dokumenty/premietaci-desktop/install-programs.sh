e04a2a81768eb7a3ab9a4150b85c8ba04927394f
Ivan Kohut <ivankohut@azet.sk>
2026-01-13 19:21:20 +0100

    Použitie dlhých volieb príkazov

--- dokumenty/premietaci-desktop/install-programs.sh
+++ dokumenty/premietaci-desktop/install-programs.sh
@@ -20,7 +20,7 @@ function create-menu-entry {
   EXEC_COMMAND=$2
   ICON_PATH=$3
   MENU_ENTRIES_DIR=~/.local/share/applications
-  mkdir -p $MENU_ENTRIES_DIR
+  mkdir --parents $MENU_ENTRIES_DIR
   cat > $MENU_ENTRIES_DIR/${APPLICATION_NAME}.desktop << EOF
 [Desktop Entry]
 Comment=
@@ -41,7 +41,7 @@ EOF
 
 function install-video-software {
   # VLC (to have additional multimedia codecs), ffmpeg, gstreamer and smplayer from _packman_ repository:
-  sudo zypper addrepo -cfp 90 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_${OPENSUSE_LEAP_VERSION} packman
+  sudo zypper addrepo --check --refresh --priority 90 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_${OPENSUSE_LEAP_VERSION} packman
   sudo zypper --gpg-auto-import-keys refresh
   sudo zypper dist-upgrade -y --from packman --allow-downgrade --allow-vendor-change
   sudo zypper install -y --from packman ffmpeg gstreamer-plugins-bad gstreamer-plugins-libav gstreamer-plugins-ugly vlc vlc-codecs smplayer
@@ -56,12 +56,12 @@ function install-video-software {
 function install-deskreen {
   DESKREEN_DIR="$1"
   DESKREEN_FILE=Deskreen.AppImage
-  curl -SLo $DESKREEN_FILE https://www.dropbox.com/scl/fi/sorz2egjuqnemowmdmqaf/Deskreen-2.0.4.AppImage?rlkey=licmgk2rcufdzrknfn8flkd4y&st=cg9f4xq2&dl=0
+  curl --show-error --location --output $DESKREEN_FILE https://www.dropbox.com/scl/fi/sorz2egjuqnemowmdmqaf/Deskreen-2.0.4.AppImage?rlkey=licmgk2rcufdzrknfn8flkd4y&st=cg9f4xq2&dl=0
   chmod +x $DESKREEN_FILE
-  mkdir -p "$DESKREEN_DIR"
+  mkdir --parents "$DESKREEN_DIR"
   mv $DESKREEN_FILE "$DESKREEN_DIR"
   DESKREEN_ICON=deskreen.png
-  curl -SLo $DESKREEN_ICON https://www.dropbox.com/scl/fi/xkbe1jgkpjz3sgpbv243n/deskreen-logo-icon_512x512.png?rlkey=243u3siohoqcbaavpxep12bkp&st=wht81lwe&dl=0
+  curl --show-error --location --output $DESKREEN_ICON https://www.dropbox.com/scl/fi/xkbe1jgkpjz3sgpbv243n/deskreen-logo-icon_512x512.png?rlkey=243u3siohoqcbaavpxep12bkp&st=wht81lwe&dl=0
   mv $DESKREEN_ICON "$DESKREEN_DIR"
   create-menu-entry Deskreen "$DESKREEN_DIR/$DESKREEN_FILE" "$DESKREEN_DIR/$DESKREEN_ICON"
   sudo firewall-cmd --permanent --add-port=3131/tcp
@@ -76,8 +76,8 @@ function install-openlp {
   OPENLP_VERSION=3.1.7
 
   # Install Python environment
-  mkdir -p "$OPENLP_DIR"
-  sudo zypper -n install python313-devel gcc-c++ libicu-devel dbus-1-devel glib2-devel
+  mkdir --parents "$OPENLP_DIR"
+  sudo zypper install -y python313-devel gcc-c++ libicu-devel dbus-1-devel glib2-devel
   python3.13 -m venv "$OPENLP_DIR/.venv"
   source "$OPENLP_DIR/.venv/bin/activate"
   python -m pip install --upgrade pip
@@ -86,8 +86,8 @@ function install-openlp {
   # Install OpenLP
   OPENLP_PACKAGE="OpenLP-$OPENLP_VERSION.tar.gz"
   wget https://get.openlp.org/$OPENLP_VERSION/$OPENLP_PACKAGE
-  tar -xvzf $OPENLP_PACKAGE -C "$OPENLP_DIR"
-  rm -f $OPENLP_PACKAGE
+  tar --extract --gzip --verbose --file="$OPENLP_PACKAGE" --directory="$OPENLP_DIR"
+  rm --force $OPENLP_PACKAGE
 
   # Create run script
   cat > "$OPENLP_DIR/run.sh" <<EOL
@@ -115,11 +115,11 @@ function install-obs-with-ndi {
   DISTROAV_VERSION=6.1.1
   DISTROAV_DEB=distroav-${DISTROAV_VERSION}-x86_64-linux-gnu.deb
   wget https://github.com/DistroAV/DistroAV/releases/download/${DISTROAV_VERSION}/${DISTROAV_DEB}
-  alien -r ${DISTROAV_DEB}
+  alien --to-rpm ${DISTROAV_DEB}
   DISTROAV_RPM=distroav-${DISTROAV_VERSION}-2.x86_64.rpm
   # Using RPM directly in order to break missing dependency in a scriptable way (zypper does not allow that)
-  sudo rpm -i --nodeps --replacefiles --replacepkgs ${DISTROAV_RPM}
-  sudo ln -f -s /usr/lib/x86_64-linux-gnu/obs-plugins/distroav.so /usr/lib64/obs-plugins/distroav.so
+  sudo rpm --install --nodeps --replacefiles --replacepkgs ${DISTROAV_RPM}
+  sudo ln --force --symbolic /usr/lib/x86_64-linux-gnu/obs-plugins/distroav.so /usr/lib64/obs-plugins/distroav.so
   rm ${DISTROAV_RPM} ${DISTROAV_DEB}
 
   # Firewall setup
@@ -141,20 +141,20 @@ function install-obs-with-ndi {
 
 function install-xnview-classic {
   DESTINATION_DIR=$1
-  mkdir -p $DESTINATION_DIR
+  mkdir --parents $DESTINATION_DIR
   PACKAGE_FILE=xnview.zip
-  curl -SLo ${PACKAGE_FILE} https://www.xnview.com/download.php?file=XnView-win-full.zip
+  curl --show-error --location --output ${PACKAGE_FILE} https://www.xnview.com/download.php?file=XnView-win-full.zip
   7z x ${PACKAGE_FILE}
   mv XnView/* -t ${DESTINATION_DIR}
   EXE_PATH="${DESTINATION_DIR}/xnview.exe"
   ICON_PATH="${DESTINATION_DIR}/xnview.ico"
-  wrestool -x -t 14 "${EXE_PATH}" > "${ICON_PATH}"
+  wrestool --extract --type=14 "${EXE_PATH}" > "${ICON_PATH}"
   WINEPREFIX="$HOME/.wine-xnview"
   create-menu-entry XnView "flatpak run --env=\"WINEPREFIX=${WINEPREFIX}\" ${WINE_FLATPAK} ${EXE_PATH}" "${ICON_PATH}"
   XNVIEW_CONFIG_DIR="$WINEPREFIX/drive_c/users/${USER}/AppData/Roaming/XnView"
-  mkdir -p $XNVIEW_CONFIG_DIR
+  mkdir --parents $XNVIEW_CONFIG_DIR
   cp xnview.ini $XNVIEW_CONFIG_DIR/xnview.ini
-  rm -d XnView
+  rm --dir XnView
   rm ${PACKAGE_FILE}
 }
 
@@ -164,16 +164,16 @@ function clone-git-repo {
 
 function install-opensong {
   DESTINATION_DIR=$1
-  mkdir -p $DESTINATION_DIR
+  mkdir --parents $DESTINATION_DIR
   sudo zypper install -y git
   PACKAGE_FILE=opensong.7z
-  curl -SLo ${PACKAGE_FILE} "https://www.dropbox.com/scl/fi/7v2ynw8kqugre0d7xdbxc/opensong-portable-configured-nocontent.7z?rlkey=zxctstsxegfgg7akv2scyzwfb&st=dd3b1s9m&dl=0"
+  curl --show-error --location --output ${PACKAGE_FILE} "https://www.dropbox.com/scl/fi/7v2ynw8kqugre0d7xdbxc/opensong-portable-configured-nocontent.7z?rlkey=zxctstsxegfgg7akv2scyzwfb&st=dd3b1s9m&dl=0"
   7z x ${PACKAGE_FILE}
-  mv OpenSong-portable/* -t ${DESTINATION_DIR}
+  mv OpenSong-portable/* --target-directory=${DESTINATION_DIR}
   DOCUMENTS_DIR=$(xdg-user-dir DOCUMENTS)
-  ln -snf "${DESTINATION_DIR}/OpenSong Data/Sets" "$DOCUMENTS_DIR/Sets"
+  ln --symbolic --no-dereference --force "${DESTINATION_DIR}/OpenSong Data/Sets" "$DOCUMENTS_DIR/Sets"
   SONGS_DIR="${DESTINATION_DIR}/OpenSong Data/Songs"
-  ln -snf "${SONGS_DIR}" "$DOCUMENTS_DIR/Songs"
+  ln --symbolic --no-dereference --force "${SONGS_DIR}" "$DOCUMENTS_DIR/Songs"
   clone-git-repo "${SONGS_DIR}" 400 400
   clone-git-repo "${SONGS_DIR}" bratske-piesne Bratske\ piesne
   clone-git-repo "${SONGS_DIR}" chvalte-pana-jezisa Chvalte\ Pana\ Jezisa
@@ -182,33 +182,33 @@ function install-opensong {
   clone-git-repo "${SONGS_DIR}" detske-piesne Ine/Detske\ piesne
   clone-git-repo "${SONGS_DIR}" matuzalem Ine/Matuzalem
   clone-git-repo "${SONGS_DIR}" spevokol Ine/Spevokol
-  mkdir -p "${SONGS_DIR}/Ine/Rozne"
+  mkdir --parents "${SONGS_DIR}/Ine/Rozne"
   clone-git-repo "${DESTINATION_DIR}" opensong-bibles OpenSong\ Scripture
   create-menu-entry OpenSong "flatpak run --env=\"WINEPREFIX=$HOME/.wine-opensong\" ${WINE_FLATPAK} ${DESTINATION_DIR}/OpenSong.exe" "${DESTINATION_DIR}/OpenSong2.ico"
-  rm -d OpenSong-portable
+  rm --dir OpenSong-portable
   rm ${PACKAGE_FILE}
 }
 
 function install-fonts {
   FONTS_DIR=~/.local/share/fonts
-  mkdir -p $FONTS_DIR
+  mkdir --parents $FONTS_DIR
 
   # Windows 7 fonts
   FONT_FILE=windows7-latin-ttf-fonts.7z
-  curl -Lo $FONT_FILE "https://www.dropbox.com/scl/fi/g0xt8q1yvyr0f1lg57svq/windows7-latin-ttf-fonts.7z?rlkey=nhoj1updclytnufol6012sxm0&st=5jk35cjw&dl=0"
+  curl --show-error --location --output $FONT_FILE "https://www.dropbox.com/scl/fi/g0xt8q1yvyr0f1lg57svq/windows7-latin-ttf-fonts.7z?rlkey=nhoj1updclytnufol6012sxm0&st=5jk35cjw&dl=0"
   7z x $FONT_FILE -o$FONTS_DIR
   rm $FONT_FILE
   # Bahnschrift
-  curl -Lo $FONTS_DIR/bahnschrift.ttf "https://www.dropbox.com/scl/fi/e7s5yyt7ktehix104q183/bahnschrift.ttf?rlkey=dg3ny56emrc1d6vi3hirxxn0g&st=hl5mggdn&dl=0"
+  curl --show-error --location --output $FONTS_DIR/bahnschrift.ttf "https://www.dropbox.com/scl/fi/e7s5yyt7ktehix104q183/bahnschrift.ttf?rlkey=dg3ny56emrc1d6vi3hirxxn0g&st=hl5mggdn&dl=0"
   # Centaur
-  curl -Lo $FONTS_DIR/centaur.ttf "https://www.dropbox.com/scl/fi/s71zpy0qqsoduci5oclla/centaur.ttf?rlkey=upgrn367tfoh86flsnotxzbqt&st=k7nbp1vz&dl=0"
+  curl --show-error --location --output $FONTS_DIR/centaur.ttf "https://www.dropbox.com/scl/fi/s71zpy0qqsoduci5oclla/centaur.ttf?rlkey=upgrn367tfoh86flsnotxzbqt&st=k7nbp1vz&dl=0"
   # Century Gothic
   FONT_FILE=century-gothic-cufonfonts.zip
-  curl -Lo $FONT_FILE "https://www.dropbox.com/scl/fi/9yap9hr7qn1g858950ko3/century-gothic-cufonfonts.zip?rlkey=lyxjwu6dhewh2esw4kgpqz0er&st=1jzjzzlg&dl=0"
+  curl --show-error --location --output $FONT_FILE "https://www.dropbox.com/scl/fi/9yap9hr7qn1g858950ko3/century-gothic-cufonfonts.zip?rlkey=lyxjwu6dhewh2esw4kgpqz0er&st=1jzjzzlg&dl=0"
   unzip $FONT_FILE -d $FONTS_DIR
   rm $FONT_FILE
 
-  fc-cache -f -v
+  fc-cache --force --verbose
 }
 
 function install-firefox {
@@ -216,10 +216,10 @@ function install-firefox {
   firefox --headless --createprofile default
   PROFILE_DIR=$(find ~/.mozilla/firefox/ -iname *.default)
   EXTENSIONS_DIR=$PROFILE_DIR/extensions
-  mkdir -p $EXTENSIONS_DIR
-  curl -Lo $EXTENSIONS_DIR/{b9db16a4-6edc-47ec-a1f4-b86292ed211d}.xpi https://addons.mozilla.org/firefox/downloads/file/4502183/video_downloadhelper-9.5.0.2.xpi
+  mkdir --parents $EXTENSIONS_DIR
+  curl --show-error --location --output $EXTENSIONS_DIR/{b9db16a4-6edc-47ec-a1f4-b86292ed211d}.xpi https://addons.mozilla.org/firefox/downloads/file/4502183/video_downloadhelper-9.5.0.2.xpi
   # Video Download Helper CoApp
-  curl -sSLf https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
+  curl --silent --show-error --location --fail https://github.com/aclap-dev/vdhcoapp/releases/latest/download/install.sh | bash
 }
 
 function install-optional-software {
@@ -257,7 +257,7 @@ function sed-appletsrc {
   echo "Ensuring appletsrc contains $2"
   while ! perl -0777 -pe "exit 1 unless /$2/s" "$FILE"; do
     echo "Updating appletsrc"
-    sed -i "s/^plugin=$1/plugin=$1\n\n$2/" "$FILE"
+    sed --in-place "s/^plugin=$1/plugin=$1\n\n$2/" "$FILE"
     sleep 3s
   done
 }
@@ -334,15 +334,15 @@ function os-configuration {
 
   # GRUB
   ## Set GRUB timeout to 0
-  sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-  grep -q '^GRUB_TIMEOUT=' /etc/default/grub || echo 'GRUB_TIMEOUT=0' | sudo tee -a /etc/default/grub
+  sudo sed --in-place 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+  grep -q '^GRUB_TIMEOUT=' /etc/default/grub || echo 'GRUB_TIMEOUT=0' | sudo tee --append /etc/default/grub
   ## Enable running of 32 bit apps such as OpenSong or XnView
   sudo /usr/sbin/update-bootloader --add-option "ia32_emulation=1"
   ## Write GRUB configuration
   if [ -d /sys/firmware/efi ]; then
-    sudo grub2-mkconfig -o /boot/efi/EFI/opensuse/grub.cfg
+    sudo grub2-mkconfig --output=/boot/efi/EFI/opensuse/grub.cfg
   else
-    sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+    sudo grub2-mkconfig --output=/boot/grub2/grub.cfg
   fi
 
   # Disable delta RPM downloads
@@ -353,7 +353,7 @@ function os-configuration {
 OPENSUSE_LEAP_VERSION=16.0
 WINE_FLATPAK=org.winehq.Wine
 PROGRAMS_DIR=~/programs
-mkdir -p $PROGRAMS_DIR
+mkdir --parents $PROGRAMS_DIR
 echo "Sudo is required for installation of some packages via zypper"
 
 # Global - OS configuration, software installed for all users
